23e4939e97153d23a1796175666443a20da14a9e
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2022-07-25 10:18:15 +0200

    use v-if

---
+++ 5/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+  data() {
+    return {
+    enteredValue:'',
+    goals: []
+    };
+  },
+  methods :{
+    addGoal(){
+      this.goals.push(this.enteredValue)
+    }
+  }
+});
+
+app.mount('#user-goals');
