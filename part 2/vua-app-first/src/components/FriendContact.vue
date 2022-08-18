e77d1a3da624bae45f7b3201e6b907b75cf509ab
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2022-08-18 16:53:12 +0200

    initial commit

--- part 2/vua-app-first/src/components/FriendContact.vue
+++ part 2/vua-app-first/src/components/FriendContact.vue
@@ -1,16 +1,24 @@
 <template>
   <li>
-    <h2>{{ friends.name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite === '1' ? '(Favorite)' : ''}}</h2>
+    <button @click="toggleFavorite">toggle favorite</button>
     <button @click="toggleDetailsVisable">ShowDetails</button>
     <ul v-if="detalisVisable">
-      <li>Phone Number: {{friends.phone}}</li>
-      <li>Phone Number: {{friends.email}}</li>
+      <li>Phone Number: {{phoneNumber}}</li>
+      <li>Email: {{emailAddress}}</li>
     </ul>
   </li>
 </template>
 
 <script>
+
 export default {
+  props: [
+  'name',
+  'phoneNumber',
+  'emailAddress',
+  'isFavorite'
+  ],
   data() {
     return {
       detalisVisable: false,
@@ -20,12 +28,21 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
+      friendIsFavorite: this.isFavorite
     };
   },
 
   methods: {
   toggleDetailsVisable(){
     this.detalisVisable = !this.detalisVisable
+  },
+  toggleFavorite(){
+    if(this.friendIsFavorite === '1'){
+      this.friendIsFavorite == '0'
+    }else {
+      this.friendIsFavorite == '1'
+    }
+    // this.isFavorite = !this.isFavorite
   }
     
   },
