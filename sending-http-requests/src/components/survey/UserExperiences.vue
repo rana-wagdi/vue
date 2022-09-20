bd6a9ab50681ff356e17257dbde7e0c993e8f656
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2022-09-20 13:31:12 +0200

    getData

---
+++ sending-http-requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,65 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperince">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+
+  components: {
+    SurveyResult,
+  },
+  data(){
+    return{
+      results: []
+    }
+  },
+  methods: {
+      loadExperince(){
+                console.log("data")
+
+         fetch('https://vue-http-demo-81210-default-rtdb.firebaseio.com/survey.json')
+         .then((response) =>{
+          if(response.ok){
+            return response.json()
+          }
+         }).then((data)=>{
+          const results = []
+          for(const id in data){
+          results.push({
+           id:id,
+            name:data[id].name,
+            rating: data[id].rating
+          })
+          }
+          this.results = results
+         
+         })
+         }
+},
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
