f9d556a03877bce1bef5d43fbe30f6796f78cfc1
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2022-10-09 11:10:29 +0200

    navigation

--- router/src/components/teams/TeamMembers.vue
+++ router/src/components/teams/TeamMembers.vue
@@ -16,17 +16,32 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: ['users', 'teams'],
   components: {
-    UserItem
+    UserItem,
   },
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      teamName: '',
+      members: [],
     };
+  },
+  created() {
+    // this.$route.path  //teams/t1
+    const teamId = this.$route.params.teamId;
+    console.log(teamId);
+    const selectedTeam = this.teams.find((team) => team.id === teamId);
+    console.log("selected", selectedTeam)
+    const members = selectedTeam.members;
+    console.log(members)
+    const selectedMembers = [];
+
+    for (const member of members) {
+      const selectedUser = this.users.find((user) => user.id === member);
+      selectedMembers.push(selectedUser);
+    }
+    this.members = selectedMembers;
+    this.teamName = selectedTeam.name;
   },
 };
 </script>
