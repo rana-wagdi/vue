4c3802d441ff40e96a9ecda4f091003e77c932ea
Rana Wagdi <73128713+rana-wagdi@users.noreply.github.com>
2022-10-31 13:18:59 +0200

    vuex

--- vuex/src/components/TheCounter.vue
+++ vuex/src/components/TheCounter.vue
@@ -1,11 +1,11 @@
 <template>
-    <h3>{{counter}}</h3>
+    <h3>{{ counter }}</h3>
 </template>
 <script>
 export default {
   computed:{
     counter(){
-      return this.$store.state.counter
+      return this.$store.getters.finalCounter
     }
   },
 }   
---
+++ vuex/src/components/UserAuth.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="logIn" v-if="!isAuth">Login</button>
+    <button @click="logOut" v-if="isAuth">Logout  </button>
+</template>
+
+<script>
+    export default {
+        methods: {
+            logIn(){
+                this.$store.dispatch('loggedIn')
+            },
+            logOut(){
+                this.$store.dispatch('loggedOut')
+            }
+        },
+        computed: {
+            isAuth() {
+                return this.$store.getters.userIsAuthenticated
+            }
+        }
+    }
+</script>
