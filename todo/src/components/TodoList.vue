a3fc4f409d1927844461ca2bf6d5cac1a277535a
Rana Wagdi <>
2022-12-06 11:23:54 +0200

    todo filter

--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <goal-todo :todoLists="todoLists"></goal-todo>
+    <div class="goal">
+      <div @click="resetTask" class="circle_goal">
+        <span class="circle_goal_item"></span>
+      </div>
+      <input
+        class="goal"
+        type="text"
+        ref="goal"
+        v-model="enterGoal"
+        placeholder="Create a new todo..."
+        @keyup.enter="addTask"
+      />
+    </div>
+    <!-- <goal-todo  :todoLists="todoLists"></goal-todo> -->
     <ul>
       <draggable>
         <li class="task_list" v-for="list in setFilter(type)" :key="list.id">
@@ -18,20 +31,25 @@
           ></span>
         </li>
       </draggable>
-      <footer-todo :listLength="itemLength" @set="setFilter"></footer-todo>
+      <footer-todo
+        @removeTodo="removeAllCheckedTodo"
+        :listLength="itemLength"
+        @set="setFilter"
+      ></footer-todo>
     </ul>
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
-import GoalTodo from "./GoalTodo.vue";
+// import GoalTodo from "./GoalTodo.vue";
 import FooterTodo from "./FooterTodo.vue";
 
 export default {
   components: {
     draggable,
-    GoalTodo,
+
+    // GoalTodo,
     FooterTodo,
   },
   data() {
@@ -55,49 +73,58 @@ export default {
       ],
       itemLength: 0,
       type: "all",
+      enterGoal: "",
     };
   },
   watch: {},
   methods: {
+    addTask() {
+      const enterValue = this.$refs.goal.value;
+      if (this.enterGoal.length > 0) {
+        this.todoLists.push({
+          id: new Date().toISOString(),
+          task: enterValue,
+          completed: false,
+        });
+      } else {
+        alert("Please Insert Some Goals");
+      }
+      this.enterGoal = "";
+    },
+    resetTask() {
+      this.enterGoal = "";
+    },
     removeTask(taskId) {
       const getTaskId = this.todoLists.find((ele) => ele.id == taskId);
       const todoELePosition = this.todoLists.indexOf(getTaskId);
       this.todoLists.splice(todoELePosition, 1);
     },
+    removeAllCheckedTodo() {
+      var result = confirm("Do you want Delete completed TODO are you sure? ");
+      if (result) {
+        this.todoLists = this.todoLists.filter((ele) => !ele.completed);
+        // removeCompletedTodo
+        // this.todoLists = removeCompletedTodo
+      }
+    },
     setFilter(type) {
       this.type = type;
-      if (type === "clear") {
-        this.todoLists.forEach((ele) => {
-          if (ele.completed) {
-            let indexOfCompletedTodo = this.todoLists.indexOf(ele);
-            var result = confirm('Do you want Delete completed TODO are you sure? ')
-            if(result){
-              this.todoLists.splice(indexOfCompletedTodo, 1);
-
-            }
-          }
-          this.type = "all";
-          return this.todoLists;
-        });
-      } else
-        return this.todoLists.filter((ele) => {
-          switch (type) {
-            case "all":
-              return ele;
-            case "complete":
-              return ele.completed;
-            case "active":
-              return !ele.completed;
-          }
-        });
-    },
-    clearCompleted() {
-      return this.todoLists.filter((ele) => !ele.completed);
+      return this.todoLists.filter((ele) => {
+        switch (type) {
+          case "all":
+            return ele;
+          case "complete":
+            return ele.completed;
+          case "active":
+            return !ele.completed;
+        }
+      });
     },
   },
   mounted() {
     let itemLen = this.todoLists.filter((ele) => !ele.completed);
     this.itemLength = itemLen.length;
+   
   },
   updated() {
     let itemLen = this.todoLists.filter((ele) => !ele.completed);
