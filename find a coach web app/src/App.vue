12dd24c2488c41056d9dc786fe7e470703d6b6a8
Rana Wagdi <>
2023-01-12 13:12:11 +0200

    coach item

--- find a coach web app/src/App.vue
+++ find a coach web app/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-    <router-view></router-view>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
