2e3a8d9cde0fc5724ef04aa9607f58957e44f247
rana-wagdi <ranawagdi4@gmail.com>
2023-01-25 16:22:12 +0200

    initial cmmit

---
+++ form-task/form-task/src/views/HomeItems.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>hello</h1>
+  <form>
+    <component  :is="this.stepsList[this.currentStep]"> </component>
+    <p @click="pervious">back</p>
+    <p @click="next">to</p>
+  </form>
+</template>
+<script>
+import StepOne from '@/components/StepOne.vue';
+import StepTwo from '@/components/StepTwo.vue'
+
+export default {
+    components:{
+        StepOne,
+        StepTwo
+    },
+  data() {
+    return {
+      currentStep: 0,
+      stepsList: ["StepOne",'StepTwo'],
+    };
+  },
+  methods:{
+    pervious(){
+        if(this.currentStep > 0){
+        this.currentStep--
+        }
+    },
+    next(){
+        if(this.currentStep < 1){
+        this.currentStep++
+        }
+        
+    }
+
+  }
+};
+</script>
